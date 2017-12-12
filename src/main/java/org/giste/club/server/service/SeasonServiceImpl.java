6b3608187a344a5876ca84dada671b1542ca63aa
Giste Trappiste <giste.trappiste@gmail.com>
2017-12-12 20:17:43 +0100

    Implemented findCurrent() in service

--- src/main/java/org/giste/club/server/service/SeasonServiceImpl.java
+++ src/main/java/org/giste/club/server/service/SeasonServiceImpl.java
@@ -1,7 +1,10 @@
 package org.giste.club.server.service;
 
+import java.util.Optional;
+
 import org.giste.club.common.dto.SeasonDto;
 import org.giste.club.server.entity.Season;
+import org.giste.club.server.repository.SeasonRepository;
 import org.giste.spring.data.repository.CrudRepository;
 import org.giste.spring.rest.server.service.CrudServiceImpl;
 import org.giste.spring.rest.server.service.EntityMapper;
@@ -21,8 +24,19 @@ public class SeasonServiceImpl extends CrudServiceImpl<SeasonDto, Season> implem
 
 	@Override
 	public SeasonDto findCurrent() throws EntityNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Optional<Season> season = getRepository().findFirstByOrderByYearDesc();
+		
+		if (!season.isPresent()) {
+			final String code = localeMessage.getMessage("error.club.server.baseError")
+					+ localeMessage.getMessage("error.entityNotFound.season.current.code");
+			final String message = localeMessage.getMessage("error.entityNotFound.season.current.message");
+			final String developerInfo = localeMessage.getMessage("error.entityNotFound.season.current.developerInfo");
+
+			throw new EntityNotFoundException(null, code, message, developerInfo);
+		}
+		
+		return getEntityMapper().toDto(season.get());
 	}
 
 	@Override
@@ -34,6 +48,11 @@ public class SeasonServiceImpl extends CrudServiceImpl<SeasonDto, Season> implem
 		final String developerInfo = localeMessage.getMessage("error.entityNotFound.season.id.developerInfo");
 
 		return new EntityNotFoundException(id, code, message, developerInfo);
+	}
+
+	@Override
+	protected SeasonRepository getRepository() {
+		return (SeasonRepository) super.getRepository();
 	}
 
 }
--- src/test/java/org/giste/club/server/service/SeasonServiceImplTest.java
+++ src/test/java/org/giste/club/server/service/SeasonServiceImplTest.java
@@ -1,12 +1,21 @@
 package org.giste.club.server.service;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.giste.club.common.dto.SeasonDto;
 import org.giste.club.server.entity.Season;
 import org.giste.club.server.repository.SeasonRepository;
 import org.giste.spring.data.repository.CrudRepository;
 import org.giste.spring.rest.server.service.CrudServiceImplTest;
 import org.giste.spring.rest.server.service.EntityMapper;
+import org.giste.spring.rest.server.service.exception.EntityNotFoundException;
 import org.giste.spring.util.locale.LocaleMessage;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -44,4 +53,27 @@ public class SeasonServiceImplTest extends CrudServiceImplTest<SeasonDto, Season
 		return new SeasonEntityMapper();
 	}
 
+	@Test
+	public void findCurrentIsOk() {
+		Season season = new Season(1L, 2017);
+		when(repository.findFirstByOrderByYearDesc()).thenReturn(Optional.of(season));
+		
+		SeasonDto dto = getTestService().findCurrent();
+		
+		assertThat(dto.getId(), is(season.getId()));
+		assertThat(dto.getYear(), is(season.getYear()));
+	}
+	
+	@Test
+	public void findCurrentEntityNotFound() {
+		when(repository.findFirstByOrderByYearDesc()).thenThrow(new EntityNotFoundException(null, "Code", "Message", "Developer info"));
+		
+		try {
+			getTestService().findCurrent();
+			fail("EntityNotFoundException expected");
+		}
+		catch (EntityNotFoundException e) {
+			assertThat(e.getCode(), is("Code"));
+		}
+	}
 }
